d59a6b8f964ee1feb43ef7b167942750d5ae1f71
Japhet <japhetisalingu@gmail.com>
2025-04-16 20:33:12 +0300

    Update alyrics.js

--- commandes/alyrics.js
+++ commandes/alyrics.js
@@ -42,7 +42,7 @@ zokou({
 
         // Format the message to send to the user
         const formattedMessage = `
-*RAHMANI XMD LYRICS FINDER*
+*JAPHET-MD 67 LYRICS FINDER*
 *Title:* ${title}
 *Artist:* ${artist}
 
