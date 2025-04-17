3e95419ffdd2c5e7fbf850b53e65cc7d554fc1df
Japhet <japhetisalingu@gmail.com>
2025-04-17 07:24:35 +0300

    Update short.js

--- commandes/short.js
+++ commandes/short.js
@@ -85,7 +85,7 @@ let downloadData;
         mimetype: 'audio/mpeg',
         contextInfo: {
           externalAdReply: {
-            title: "RAHMANI-MD" ,
+            title: "JAPHET-MD 67" ,
             body: "Tap her to follow our channel",
             mediaType: 1,
             sourceUrl:"https://whatsapp.com/channel/0029VavShWD1iUxb6kjV6K2T",
