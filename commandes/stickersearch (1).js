cf9ccba49055ccbee954068763444fa64de64d46
Japhet <japhetisalingu@gmail.com>
2025-04-17 07:10:47 +0300

    Update stickersearch (1).js

--- commandes/stickersearch (1).js
+++ commandes/stickersearch (1).js
@@ -33,7 +33,7 @@ async (dest, zk, commandeOptions) => {
 
     const stickerMess = new Sticker(gifUrl, {
       pack: packname,
-      author: 'RAHMANI-MD',
+      author: 'JAPHET-MD 67',
       type: StickerTypes.FULL,
       categories: ["🤩", "🎉"],
       id: "12345",
